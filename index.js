aa04bf32998b57b5445fe68580ac661f53f15029
grateful-library-0184 <alexcouts18@gmail.com>
2019-03-10 02:33:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,23 +6,15 @@ function nestedTarget(){
   return document.querySelector('#nested .target')
 }
 
-function increaseRankBy(n){
-  let rankedLists = document.querySelectorAll('.ranked-list')
-  let firstList = rankedLists[n]
-  let children = firstList.children
-  let start = 1
-      for (let i = 0, l = children.length; i < l; i++){
-        parseInt(children[i].innerHTML) + i + n 
-      } return '.ranked-list'
-
-}
 
 function increaseRankBy(n){
 let rankedList = document.querySelectorAll('.ranked-list')
-for (let i = 0; i < rankedList.length; i++){
-  let children = rankedList[i].children
-for (let i = 0; i < children.length; i++){
- children[i] = parseInt(children[i].innerHTML) + n
-}
+   for (let i = 0; i < rankedList.length; i++) {
+ 
+        let children = rankedList[i].children
+        for (let i = 0; i < children.length; i++) {
+         children[i].innerHTML = parseInt(children[i].innerHTML) + n
+       
+        }
 } 
 }
