38810eed795f9fb9c064234676591af3f012c1c6
grateful-library-0184 <alexcouts18@gmail.com>
2019-03-10 01:22:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  let rankedLists = document.querySelectorAll('.ranked-list')
+  let firstList = rankedLists[n]
+  let children = firstList.children
+  let start = 1
+      for (let i = 0, l = children.length; i < l; i++){
+        parseInt(children[i].innerHTML) + i + n 
+      } return '.ranked-list'
+
+}
+
+function increaseRankBy(n){
+let rankedList = document.querySelectorAll('.ranked-list')
+for (let i = 0; i < rankedList.length; i++){
+  let children = rankedList[i].children
+for (let i = 0; i < children.length; i++){
+ children[i] = parseInt(children[i].innerHTML) + n
+}
+} 
+}
