0c325a12b41d0663888f695504163cbc7dc84891
grateful-library-0184 <alexcouts18@gmail.com>
2019-03-10 03:00:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ let rankedList = document.querySelectorAll('.ranked-list')
 } 
 }
 
-function deppestChild(){
+function deepestChild(){
   let deepChild = document.querySelector('.grand-node')
   for (let i = 0; i < deepChild.length; i++){
     return deepchild[3].innerHTML
